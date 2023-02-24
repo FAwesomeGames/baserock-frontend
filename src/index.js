1ac78526fe72239cfcf8376bd0633c1beb96811d
Michael <Iysbaera@gmail.com>
2023-02-24 07:39:30 +0600

    feat: moved contract address above title

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ function App() {
   return (
     <div className="center">
       <h1>BaseRock</h1>
-      <img src="/rock.png" alt="rock" />
-
       <div className="contract-address">
         <p className="highlighted">Contract Address:</p>
         <p><a href="https://etherscan.io/address/0x26a65db78c34291bf272d5eab441b4dda88da9eb" target="_blank">0x26a65db78c34291bf272d5eab441b4dda88da9eb</a></p>
       </div>
+      <img src="/rock.png" alt="rock" />
+
+
 
     </div>
   );
