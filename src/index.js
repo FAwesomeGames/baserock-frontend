07eeca9a1501d3f9c4262a3295f22aa80816d652
Michael <Iysbaera@gmail.com>
2023-02-28 01:57:18 +0600

    feat: added opensea

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ function App() {
   return (
     <div className="center">
       <h1>BaseRock</h1>
+
       <div className="contract-address">
+        <p className="highlighted-mega"><a href="https://opensea.io/collection/baserock" target="_blank">Live on Opensea</a></p>
         <p className="highlighted">Contract Address:</p>
         <p><a href="https://etherscan.io/address/0x26a65db78c34291bf272d5eab441b4dda88da9eb" target="_blank">0x26a65db78c34291bf272d5eab441b4dda88da9eb</a></p>
       </div>
-      <img src="/rock.png" alt="rock" />
 
-
-
+      <img className="rockImage" src="/rock.png" alt="rock" />
     </div>
   );
 }
